f5b0354f52459492f4773a26c3a0f4912071f251
Atanu Pramanik <atanupramanik506@gmail.com>
2023-12-22 23:03:24 +0530

    1st js commit

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert( 3 + 3 ); // it will work if we use browser because the engine is hidden inside the browser , but we are using nodejs
+
+let name = "atanu";
+let age = 20
+let isLoggedIn = true;
+
+// numver = 2 to power 53
+// bigint
+//string => ""
+//boolean=> true/false or yes/no
+//null => used when we want something to be nothing
+//undefined => variable which has not been assigned any value yet.
+//symbol => unique 
+//object =>
+
+console.log(typeof age);    //output => number
+console.log(typeof null)    //output => object
+console.log(typeof undefined)  //output => undefined
+
+
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,18 @@
+const accountId = 1323   // can't change
+let accountEmail = "atau33@gamil.com"
+var accountPassword = "23455544"
+accountCity = "arambagh"
+let accountState;
+
+//accoutId = 44     // not allowed
+console.log(accountId);
+
+/*
+    its better to use let , preferable not to use var because of scope block
+*/
+
+accountEmail="ata44@gamil.com"
+accountPassword="55555"
+accountCity="bangaluru"
+
+console.table([accountEmail, accountPassword, accountCity,accountState])
